82204c0b6af381a2f04e3e4a981129dba72eece7
Hassiel Monjaraz <HassielAM_012@outlook.com>
2024-05-14 07:08:29 -0600

    Calculadora

--- Calculadora/js/index.js
+++ Calculadora/js/index.js
@@ -7,12 +7,34 @@ let asignar = (valor) => resultado.value += valor;
     
 // }
 
-function calcular(valor){
-    if (resultado.valor != '')
-        resultado.value = eval(resultado.value);
-    else
-        alert('Ingrese valor');
+
+
+function calcular() {
+    try {
+        let expression = resultado.value
+
+        // Añadir operadores de multiplicación implícitos
+        expression = expression.replace(/\%/g, '**');
+        expression = expression.replace(/\^/g, '**'); // Reemplazar ^ con ** para la operación de potencia
+        expression = expression.replace(/(\d)(\()/g, '$1*('); // Entre número y paréntesis de apertura
+        expression = expression.replace(/(\))(\d)/g, ')*$2'); // Entre paréntesis de cierre y número
+        expression = expression.replace(/(\))(\()/g, ')*(');  // Entre paréntesis de cierre y paréntesis de apertura
+
+        // Evaluar la expresión y mostrar el resultado
+        resultado.value = eval(expression);
+    } catch (error) {
+        alert('Expresión inválida');
+    }
 }
+
+// function calcular(valor){
+//     if (resultado.valor != '')
+//         resultado.value = eval(resultado.value);
+//     else
+//         alert('Ingrese valor');
+// }
+
+
 
 // function limpiar(){
 //     resultado.value = '';
